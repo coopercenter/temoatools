63222af2770ea1a93bebe155c91e09a0b0050089
Jeff Bennett <jab6ft@virginia.edu>
2021-01-07 20:51:37 -0500

    Updated model inputs
    New error created with CapacityCredit, temoa_model_build will needs some modifications

--- projects/va_emerging_tech/analyze_inputs/ATB_data_analysis_2025/analyze_ATB_data_2025.py
+++ projects/va_emerging_tech/analyze_inputs/ATB_data_analysis_2025/analyze_ATB_data_2025.py
@@ -31,15 +31,15 @@ data = pd.read_excel('2020_ATB_CostProjections.xlsx', sheet_name=None)
 # prepare DataFrame to store results
 entries = [
     'Technology',
-    'CAPEX_2018',
+    'CAPEX_2025',
     'CAPEX_Advanced_rate',
     'CAPEX_Moderate_rate',
     'CAPEX_Conservative_rate',
-    'FixedOM_2018',
+    'FixedOM_2025',
     'FixedOM_Advanced_rate',
     'FixedOM_Moderate_rate',
     'FixedOM_Conservative_rate',
-    'VariableOM_2018',
+    'VariableOM_2025',
     'VariableOM_Advanced_rate',
     'VariableOM_Moderate_rate',
     'VariableOM_Conservative_rate'
@@ -59,9 +59,9 @@ for i in range(2):
         years = years.astype(float)
 
         s['Technology'] = key
-        s['CAPEX_2018'] = df.loc['CAPEX_Moderate', 2018]
-        s['FixedOM_2018'] = df.loc['FixedOM_Moderate', 2018]
-        s['VariableOM_2018'] = df.loc['VariableOM_Moderate', 2018] * 0.000001 * 277777.7778  # convert $/MWh to M$/PJ
+        s['CAPEX_2025'] = df.loc['CAPEX_Moderate', 2025]
+        s['FixedOM_2025'] = df.loc['FixedOM_Moderate', 2025]
+        s['VariableOM_2025'] = df.loc['VariableOM_Moderate', 2025] * 0.000001 * 277777.7778  # convert $/MWh to M$/PJ
 
         input_vars = ['CAPEX_Advanced', 'CAPEX_Moderate', 'CAPEX_Conservative',
                       'FixedOM_Advanced', 'FixedOM_Moderate', 'FixedOM_Conservative',
@@ -70,26 +70,28 @@ for i in range(2):
                        'FixedOM_Advanced_rate', 'FixedOM_Moderate_rate', 'FixedOM_Conservative_rate',
                        'VariableOM_Advanced_rate', 'VariableOM_Moderate_rate', 'VariableOM_Conservative_rate']
         for input_var, output_var in zip(input_vars, output_vars):
-            if df.loc[input_var, 2018] > 0.0:
-                if i==0: # Design curve fit to match end year
+            if df.loc[input_var, 2025] > 0.0:
+                if i == 0:  # Design curve fit to match end year
                     curve_fit_type = 'endYear'
-                    y_2018 = df.loc[input_var, 2018]
+                    y_2025 = df.loc[input_var, 2025]
                     y_2050 = df.loc[input_var, 2050]
-                    exponent = np.log(y_2050/y_2018) / (2050-2018)
-                elif i==1: # optimized curve fit, lowest R2 value across all years
+                    exponent = np.log(y_2050 / y_2025) / (2050 - 2025)
+                    s[output_var] = exponent
+
+                elif i == 1:  # optimized curve fit, lowest R2 value across all years
                     curve_fit_type = 'optimized'
-                    ydata = df.loc[input_var, :].values / df.loc[input_var, 2018]
+                    ydata = df.loc[input_var, :].values / df.loc[input_var, 2025]
                     popt, pcov = curve_fit(func, years, ydata)
                     exponent = popt[0]
                     s[output_var] = exponent
 
                 # Plot one validation case for each technology
-                if input_var in ['CAPEX_Advanced', 'CAPEX_Moderate', 'CAPEX_Conservative']:
+                if input_var in ['CAPEX_Moderate']:
                     plt.figure()
                     plt.plot(years, df.loc[input_var, :].values, 'k.')
-                    fit = df.loc[input_var, 2018] * np.exp(exponent * years)
+                    fit = df.loc[input_var, 2025] * np.exp(exponent * years)
                     plt.plot(years, fit, 'b--')
-                    save_name = curve_fit_type + '_curve_fit_' + input_var + '_' + key + '.png'
+                    save_name = key + '_curve_fit_' + input_var + '_' + curve_fit_type + '.png'
                     plt.xlabel('Year')
                     plt.ylabel('CAPEX ($/kW)')
                     plt.ylim(bottom=0.0)
